6cfc93cda8b53e528a9561acc53df2fc62054afb
Nika Jones <nika.jones@sta.samsung.com>
2013-04-29 16:01:30 -0700

    Including the MySQL helper class.

--- cookbooks/x_server/recipes/add_email_v1.rb
+++ cookbooks/x_server/recipes/add_email_v1.rb
@@ -1,11 +1,15 @@
 rightscale_marker :begin
 
+  class Chef::Recipe
+    include RightScale::Database::MySQL::Helper
+  end
+
   log "Starting to add the email address"
 
   full_name = node[:x_server][:full_name]
   email_address = node[:x_server][:email_address]
 
-  RightScale::Database::MySQL::Helper.do_query(node, "INSERT INTO USERS (CREATED_DATE, USER_EMAIL, EMAIL_LOWERCASE, FULL_NAME, lastUpdateOn, TYPE, VERSION) VALUES (NOW(), '#{email_address}', '#{email_address.downcase}', '#{full_name}', NOW(), 0, 0)")
+ do_query(node, "INSERT INTO USERS (CREATED_DATE, USER_EMAIL, EMAIL_LOWERCASE, FULL_NAME, lastUpdateOn, TYPE, VERSION) VALUES (NOW(), '#{email_address}', '#{email_address.downcase}', '#{full_name}', NOW(), 0, 0)")
   
   log "Added the email address #{email_address} and name #{full_name} to the Database."
 

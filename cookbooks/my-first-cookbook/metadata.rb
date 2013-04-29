dea616cb0fef7abafe90a03698b0131c364dfd4b
Nika Jones <nika.jones@sta.samsung.com>
2013-04-29 10:51:01 -0700

    Added my-first-cookbook an helloworld recipe

---
+++ cookbooks/my-first-cookbook/metadata.rb
@@ -0,0 +1,9 @@
+name             'my-first-cookbook'
+maintainer       'usam'
+maintainer_email 'njones-usam@'
+license          'All rights reserved'
+description      'Installs/Configures my-first-cookbook'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+recipe "my-first-cookbook::helloworld", "My first recipe, prints Hello World to the RightScale dashboard"

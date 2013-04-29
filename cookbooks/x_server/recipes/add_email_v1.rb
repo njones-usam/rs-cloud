1f14af8f5c9e84ec545ce608aca749abb9eab1ea
Nika Jones <nika.jones@sta.samsung.com>
2013-04-29 15:33:50 -0700

    Fixed the variable name for email address.

--- cookbooks/x_server/recipes/add_email_v1.rb
+++ cookbooks/x_server/recipes/add_email_v1.rb
@@ -3,7 +3,7 @@ rightscale_marker :begin
   log "Adding the email address #{email_address} and name #{full_name} to the Database."
 
   full_name = node[:x_server][:full_name]
-  email_addres = node[:x_server][:email_address]
+  email_address = node[:x_server][:email_address]
 
   RightScale::Database::MySQL::Helper.do_query(node, "INSERT INTO USERS (CREATED_DATE, USER_EMAIL, EMAIL_LOWERCASE, FULL_NAME, lastUpdateOn, TYPE, VERSION) VALUES (NOW(), '#{email_address}', '#{email_address.downcase}', '#{full_name}', NOW(), 0, 0)")
   

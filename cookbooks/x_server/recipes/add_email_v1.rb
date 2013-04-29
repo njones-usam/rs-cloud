8d413342d28c8dc0fda3149099360ba61af5d4c8
Nika Jones <nika.jones@sta.samsung.com>
2013-04-29 14:50:37 -0700

    Added the mysql db connection variable to the script

--- cookbooks/x_server/recipes/add_email_v1.rb
+++ cookbooks/x_server/recipes/add_email_v1.rb
@@ -2,6 +2,10 @@ rightscale_marker :begin
 
   full_name = node[:x_server][:full_name]
   email_addres = node[:x_server][:email_address]
+
+  mysql_connection_info = {:host => node[:db][:dns][:master][:fqdn],
+                         :username => node[:db][:admin][:user],
+                         :password => node[:db][:admin][:password] }
   
   log "Adding the email address #{email_address} and name #{full_name} to the Database."
   

05d7b8fe3d8f8085a42728d683ed0a8d70bfff08
Nika Jones <nika.jones@sta.samsung.com>
2013-04-29 18:56:37 -0700

    No include

--- cookbooks/x_server/recipes/add_email_v1.rb
+++ cookbooks/x_server/recipes/add_email_v1.rb
@@ -1,7 +1,5 @@
 rightscale_marker :begin
 
-  ::Chef::Recipe.send(:include, RightScale::Database::MySQL::Helper)
-
   log "Starting to add the email address"
 
   full_name = node[:x_server][:full_name]

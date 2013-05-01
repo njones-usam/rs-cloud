4454519e2592e7e0d89a996d439af81e8714cbdd
Nika Jones <nika.jones@sta.samsung.com>
2013-05-01 11:44:29 -0700

    Removing the marker

--- cookbooks/my-first-cookbook/recipes/helloworld.rb
+++ cookbooks/my-first-cookbook/recipes/helloworld.rb
@@ -1,8 +1,6 @@
-rightscale_marker :begin
 
   log "Hello World"
   log "default: #{node.default_attrs.to_s}"
   log "normal: #{node.normal_attrs.to_s}"
   log "over: #{node.override_attrs.to_s}"
 
-rightscale_marker :end

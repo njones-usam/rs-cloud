f965e82739383d1165914b941ff5ad84b96be09d
Nika Jones <nika.jones@sta.samsung.com>
2013-05-01 11:27:28 -0700

    Logging attributes

--- cookbooks/my-first-cookbook/recipes/helloworld.rb
+++ cookbooks/my-first-cookbook/recipes/helloworld.rb
@@ -1,1 +1,8 @@
-log "Hello World"
+rightscale_marker :begin
+
+  log "Hello World"
+  log "default: #{node.default_attrs.to_s}"
+  log "normal: #{node.normal_attrs.to_s}"
+  log "over: #{node.override_attrs.to_s}"
+
+rightscale_marker :end

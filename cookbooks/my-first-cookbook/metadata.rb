9f2a14775fdace072b349f9809f291f590b390f2
Nika Jones <nika.jones@sta.samsung.com>
2013-05-01 11:52:49 -0700

    Re-added the rightscale marker back

--- cookbooks/my-first-cookbook/metadata.rb
+++ cookbooks/my-first-cookbook/metadata.rb
@@ -6,4 +6,6 @@ description      'Installs/Configures my-first-cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+depends "rightscale"
+
 recipe "my-first-cookbook::helloworld", "My first recipe, prints Hello World to the RightScale dashboard"
--- cookbooks/my-first-cookbook/recipes/helloworld.rb
+++ cookbooks/my-first-cookbook/recipes/helloworld.rb
@@ -1,6 +1,8 @@
+rightscale_marker :begin
 
   log "Hello World"
   log "default: #{node.default_attrs.to_s}"
   log "normal: #{node.normal_attrs.to_s}"
   log "over: #{node.override_attrs.to_s}"
 
+rightscale_marker :end

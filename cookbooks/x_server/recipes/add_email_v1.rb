da44111cd26edbffd6efcbd884727c24e2130497
Nika Jones <nika.jones@sta.samsung.com>
2013-05-01 12:41:27 -0700

    fixup! Updating the correct metadata.json files and checking to see if the db_qyery function works

--- cookbooks/x_server/recipes/add_email_v1.rb
+++ cookbooks/x_server/recipes/add_email_v1.rb
@@ -5,7 +5,7 @@ rightscale_marker :begin
   full_name = node[:x_server][:full_name]
   email_address = node[:x_server][:email_address]
 
- do_query(node, "INSERT INTO USERS (CREATED_DATE, USER_EMAIL, EMAIL_LOWERCASE, FULL_NAME, lastUpdateOn, TYPE, VERSION) VALUES (NOW(), '#{email_address}', '#{email_address.downcase}', '#{full_name}', NOW(), 0, 0)")
+ db_mysql::do_query(node, "INSERT INTO USERS (CREATED_DATE, USER_EMAIL, EMAIL_LOWERCASE, FULL_NAME, lastUpdateOn, TYPE, VERSION) VALUES (NOW(), '#{email_address}', '#{email_address.downcase}', '#{full_name}', NOW(), 0, 0)")
   
   log "Added the email address #{email_address} and name #{full_name} to the Database."
 
